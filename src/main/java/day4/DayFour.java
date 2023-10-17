d2b1ca7c317632169ccf28ec10a2e436a64a160d
Vishwa-Dinath <vishwadinath97@gmail.com>
2023-10-17 12:36:42 +0530

    Day 4 first part is completed

--- src/main/java/day4/DayFour.java
+++ src/main/java/day4/DayFour.java
@@ -1,7 +1,82 @@
 package day4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class DayFour {
     public static void main(String[] args) {
+//        System.out.println(getAssignmentPairs("/problem_4/sample_input.txt"));
 
+        System.out.println(pipeLine_1("/problem_4/sample_input.txt"));
+        System.out.println(pipeLine_1("/problem_4/input.txt"));
+    }
+
+    public static ArrayList<String> getAssignmentPairs(String filePath) {
+        InputStream is = DayFour.class.getResourceAsStream(filePath);
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(is))){
+            ArrayList<String> assignmentsPairsList = new ArrayList<>();
+            String line;
+            while ((line= br.readLine())!=null){
+                assignmentsPairsList.add(line);
+            }
+            return assignmentsPairsList;
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ArrayList<ArrayList<Range>> getAssignmentRanges(ArrayList<String> assignmentsPairsList) {
+        ArrayList<ArrayList<Range>> assignmentRangeList = new ArrayList<>();
+        for (String assignmentPair : assignmentsPairsList) {
+            String[] assignmentRanges = assignmentPair.split(",");
+            ArrayList<Range> rangeList = new ArrayList<>();
+            rangeList.add(new Range(Integer.parseInt(assignmentRanges[0].split("-")[0]),
+                    Integer.parseInt(assignmentRanges[0].split("-")[1])));
+            rangeList.add(new Range(Integer.parseInt(assignmentRanges[1].split("-")[0]),
+                    Integer.parseInt(assignmentRanges[1].split("-")[1])));
+            assignmentRangeList.add(rangeList);
+        }
+        return assignmentRangeList;
+    }
+
+    public static int findFullyContainedPairs(ArrayList<ArrayList<Range>> assignmentRangeList) {
+        int numberOfFullyContainedPairs = 0;
+        for (ArrayList<Range> ranges : assignmentRangeList) {
+            Range firstElfRange = ranges.get(0);
+            Range secondElfRange = ranges.get(1);
+            if (doesOneFullyContainOther(firstElfRange, secondElfRange)) {
+                numberOfFullyContainedPairs++;
+            }
+        }
+        return numberOfFullyContainedPairs;
+    }
+
+    public static boolean doesOneFullyContainOther(Range firstElfRange, Range secondElfRange) {
+        if ((firstElfRange.start <= secondElfRange.start && firstElfRange.end >= secondElfRange.end) ||
+                secondElfRange.start <= firstElfRange.start && secondElfRange.end >= firstElfRange.end) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int pipeLine_1(String filePath) {
+        return findFullyContainedPairs(
+                getAssignmentRanges(
+                        getAssignmentPairs(filePath)
+                )
+        );
+    }
+}
+
+class Range{
+    int start;
+    int end;
+
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
     }
 }
